8fffe7de222b10b7dc8743b249fe46a218958a8e
dima <schaefer.dietmar.de@gmail.com>
2018-03-04 21:43:49 +0100

    initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyA6BzU3uZcp6ns3NqAiZv1ghTisY7p7C6Y",
+    authDomain: "ds-oshop.firebaseapp.com",
+    databaseURL: "https://ds-oshop.firebaseio.com",
+    projectId: "ds-oshop",
+    storageBucket: "ds-oshop.appspot.com",
+    messagingSenderId: "1053943766785"
   }
 };
