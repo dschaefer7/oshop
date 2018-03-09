66b74decf968fc644ec44e62513574b25dddb64a
dima <schaefer.dietmar.de@gmail.com>
2018-03-09 06:18:16 +0100

    add userservice

---
+++ src/app/admin-auth-guard.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {CanActivate} from '@angular/router';
+import {AuthService} from './auth.service';
+import {UserService} from './user.service';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate
+{
+
+  constructor(private auth: AuthService, private userService: UserService) {
+  }
+
+
+
+  canActivate(): Observable<boolean> {
+    return this.auth.user$
+      .map(user => {
+        return this.userService.get(user.uid).valueChanges();
+      }).map(appUser => appUser.isAdmin);
+
+  }
+
+}
